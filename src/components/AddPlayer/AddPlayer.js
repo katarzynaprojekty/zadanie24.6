a76d02d3411b41b43c9c6835aff9d1851137ebd7
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-19 21:49:29 +0200

    Fix bug in AddPlayer.js

--- src/components/AddPlayer/AddPlayer.js
+++ src/components/AddPlayer/AddPlayer.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './AddPlayer.css';
 
 const AddPlayer = (props) => {
-    let input;
     const onSubmit = (event) => {
-        event.preventDefault();
-        props.onPlayerAdd(input.value);
-        input.value ='';
+      event.preventDefault();
+      props.onPlayerAdd(input.value);
+      input.value ='';
     }
-    return (
-       <form className="AddPlayer">
-           <input type="text" className="AddPlayer__input" ref={(node) => input = node}/>
-           <input type="submit" className="AddPlayer__submit" value="Add" />
-       </form>
-   )
+
+    let input;
+    
+    return(
+      <form className="AddPlayer" onSubmit={onSubmit}>
+        <input type="text" className="AddPlayer__input" ref={(node) => input = node}/>
+        <input type="submit" className="AddPlayer__submit" value="Add player" />
+      </form>
+    )
 };
 
 export default AddPlayer;
