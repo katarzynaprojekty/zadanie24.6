6839f8d74d134ad83ae686a496e761112f9f87dd
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-19 20:08:55 +0200

    Change Player.test.js - add test for name

--- src/components/Player.test.js
+++ src/components/Player.test.js
@@ -3,5 +3,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
-  shallow(<Player />);
+    shallow(<Player />);
+});
+
+it('renders correct name', () => {
+    const playerNamePassed = 'Ania';
+    const playerComponent = shallow(<Player name={playerNamePassed} />);
+  
+    const playerNameRendered = playerComponent.find('.Player__name').text();
+  
+    expect(playerNameRendered).toEqual(playerNamePassed);
 });
