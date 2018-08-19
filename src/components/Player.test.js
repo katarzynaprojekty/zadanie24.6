ff011538c578d33f61b15e90a37ccd208496b87f
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-19 20:16:50 +0200

    Change Player.test.js - add test for buttons

--- src/components/Player.test.js
+++ src/components/Player.test.js
@@ -19,9 +19,20 @@ it('should call onPlayerScoreChange with 1 when plus button is clicked', () => {
     const mockedOnPlayerScoreChange = jest.fn();
     const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
   
-    const plusButton = playerComponent.find('.Player__button');
+    const plusButton = playerComponent.find('.Player__button_plus');
   
     plusButton.simulate('click');
   
     expect(mockedOnPlayerScoreChange).toBeCalledWith(1);
+});
+
+it('should call onPlayerScoreChange with 1 when minus button is clicked', () => {
+    const mockedOnPlayerScoreChange = jest.fn();
+    const playerComponent = shallow(<Player onPlayerScoreChange={mockedOnPlayerScoreChange} />);
+  
+    const minusButton = playerComponent.find('.Player__button_minus');
+  
+    minusButton.simulate('click');
+  
+    expect(mockedOnPlayerScoreChange).toBeCalledWith(-1);
 });
