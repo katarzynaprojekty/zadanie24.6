654b1045a3a1294fb3881e75e8d7320cfecabe4a
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-19 22:12:54 +0200

    Update PlayerList.js - add remove

--- src/components/PlayersList/PlayersList.js
+++ src/components/PlayersList/PlayersList.js
@@ -4,14 +4,17 @@ import './PlayersList.css';
 
 const PlayersList = (props) => (
    <ul className="PlayersList">
-       {props.players.map((player, i) => (
+        <h1 className="Player_table">Player table:</h1>
+
+        {props.players.map((player, i) => (
            <Player
                key={i}
                name={player.name}
                score={player.score}
                onPlayerScoreChange={(points) => props.onScoreUpdate(i, points)}
+               onPlayerRemove={(i) => props.onPlayerRemove(i)}
            />)
-       )}
+        )}
    </ul>
 );
 
