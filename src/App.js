524e8cbd9b1ab13ed6da1131a38cb1304b0e6167
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-19 21:46:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 class App extends Component {
  constructor() {
@@ -31,13 +32,24 @@ class App extends Component {
     })
   }
 
- render() {
-   return (
-     <div className="App">
-       <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
-     </div>
-   );
- }
+  onPlayerAdd = (playerName) => {
+    const newPlayer = {
+      name: playerName,
+      score: 0,
+    }
+    this.setState({
+      players: [...this.state.players, newPlayer]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddPlayer onPlayerAdd={this.onPlayerAdd} />
+        <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
+      </div>
+    );
+  }
 }
 
 export default App;
