660ca5f74a79f4a169e10d70d3d2fd5d8c0cb634
Katarzyna Nieszporek <katarzyna.n.projekty@gmail.com>
2018-08-20 23:19:18 +0200

    Try add onPlayerUpdate

--- src/App.js
+++ src/App.js
@@ -48,6 +48,18 @@ class App extends Component {
     });
     console.log(this.state.players);
   };
+  
+//Brak pomysłu //czy potrzebne?
+  // onPlayerUpdate = (playerIndex) => {
+  //   this.setState({
+  //     players: this.state.players.map((player, index) => {
+  //       if (index === playerIndex) {
+  //         return { ...player};
+  //       }
+  //       return player;
+  //     })
+  //   })
+  // }
 
   render() {
     return (
@@ -55,7 +67,7 @@ class App extends Component {
         <h1 className="App_title">Scorekeeper</h1>
         
         <AddPlayer onPlayerAdd={this.onPlayerAdd} />
-        <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate} onPlayerRemove={this.onPlayerRemove} />
+        <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate} onPlayerRemove={this.onPlayerRemove} onPlayerUpdate={this.onPlayerUpdate} />
       </div>
     );
   }
